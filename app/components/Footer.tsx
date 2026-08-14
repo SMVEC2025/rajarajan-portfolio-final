056b4a58e1e4677e8e8b115511b14e2c61fd6a4b
SMVEC2025 <webdeveloper@smvec.ac.in>
2026-08-14 16:46:43 +0530

    updated

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -28,10 +28,19 @@ function LinkedInIcon() {
   );
 }
 
+function YouTubeIcon() {
+  return (
+    <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+      <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
+    </svg>
+  );
+}
+
 const SOCIAL = [
   { label: "X", href: "https://x.com/", icon: <XIcon /> },
-  { label: "Instagram", href: "https://instagram.com/", icon: <InstagramIcon /> },
-  { label: "LinkedIn", href: "https://linkedin.com/in/rajarajandhanasekaran", icon: <LinkedInIcon /> },
+  { label: "Instagram", href: "https://www.instagram.com/rajarajand_official", icon: <InstagramIcon /> },
+  { label: "LinkedIn", href: "https://www.linkedin.com/in/rajarajan-dhanasekaran-6ba5009a", icon: <LinkedInIcon /> },
+  { label: "YouTube", href: "https://www.youtube.com/@RajarajanD_Official", icon: <YouTubeIcon /> },
 ];
 
 const NAV = [
